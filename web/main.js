952f9b9df7c6fd9e525070c7a3c22d687686cff3
qypol342 <qypol342@gmail.com>
2023-03-06 11:50:01 +0100

    get list of serv

--- web/main.js
+++ web/main.js
@@ -1,7 +1,17 @@
-console.log("loaded")
+const templateServer = document.querySelector("template#server")
+const serverList = document.querySelector("#server-list")
 
 
-
-fetch('../servers.json')
-    .then((response) => response)
-    .then((json) => console.log(json));
+fetch('https://raw.githubusercontent.com/BlaBl-App/BlaBl-ist/main/servers.json')
+    .then((response) => response.json())
+    .then((json) => {
+        serverList.innerHTML =""
+        for (const server of json.servers){
+            let newServer = templateServer.content.cloneNode(true);
+            console.log("new ser",newServer)
+            console.log("new ser",newServer.innerHTML)
+            newServer.querySelector(".name").innerHTML = server.name;
+            newServer.querySelector(".ip").innerHTML = server.ip;
+            serverList.appendChild(newServer)
+        }
+        console.log(json)});
---
+++ web/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'blaBlue': '#5CBCF9',
+        'blaDarkBlue': '#1C4651',
+        'blaGreen': '#B2F37C',
+      }
+    },
+    
+  },
+  plugins: [],
+}
